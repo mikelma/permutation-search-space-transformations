5e8c9016005f7d5ea61fa26a4f2c7c9a7cafd2db
mikel <mikelma7@gmail.com>
2019-04-10 13:15:04 +0200

    vj tranformation utils and sampling added

--- optimizers/UMDA.py
+++ optimizers/UMDA.py
@@ -27,8 +27,7 @@ class UMDA():
 
         return freq
 
-    @profile
-    def sample_population(self, p, n, 
+    def sample_population_ad_hoc(self, p, n, 
                           pop=np.array([]),
                           timeout=None, 
                           quit_in_timeout=False):
@@ -95,3 +94,73 @@ class UMDA():
 
         return  putils.set2np(samples, size) 
 
+    def sample_population(self, p, n, 
+                          pop=np.array([]),
+                          timeout=None, 
+                          quit_in_timeout=False):
+        '''Given a probability matrix of size mxm, sample n solutions 
+        of length m.
+
+        Args: 
+            p (ndarray): probability matrix.
+            n (int): number of samples.
+            pop (ndarray): Default: empty array. Individuals from pop matrix 
+                           are not going to be repeated in the sampled matrix.
+                           Warning, the larger the size of pop, sampling will 
+                           be more time-consuming.
+            timeout (int or None): Default: None. Enable timeouti, in milliseconds.  
+            quit_in_timeout (bool): Default: False. Exit the program if timeout occurs.
+                                    Else, stop sampling and return obtained results.
+        
+        Returns:
+            ndarray: sampled matrix. 
+
+        '''
+        samples = set()
+        # print('p: ', p) # debug
+        size = p.shape[0] # Size of permutations
+        identity = np.array(range(size))
+
+        start = datetime.datetime.now()
+
+        while len(samples) < n:
+            delta_t = datetime.datetime.now() - start
+            if type(timeout) == int and int(delta_t.total_seconds() * 1000) >= timeout:
+                print('Warning: Timeout passed when sampling.')
+                if quit_in_timeout:
+                    quit() 
+                else:
+                    break
+            # Generate permu
+            permu = []
+            for j in range(size): # For each position
+                # Probability for elements in the j's position 
+                # p_ = np.delete(p[j], permu, axis=0) 
+                p_ = p[j]
+                # available = np.delete(identity, permu) 
+                try:
+                    if sum(p_) == 0: 
+                        rand = 0 
+                    else:
+                        rand = np.random.uniform(0, sum(p_))
+                except Exception as e:
+                    print(e, '\n', 'Program state before error:')
+                    print('p_: ', p_, ' available: ', available, '  rand: ', rand) #debug
+                    quit()
+                i = 0
+                s = 0 # sum
+                while s < rand:
+                    s += p_[i] 
+                    if s < rand:
+                        i += 1
+                # permu.append(available[i])
+                permu.append(identity[i])
+            i = 0 
+            repeated = False
+            while not repeated and i < pop.shape[0]: 
+                repeated = tuple(pop[i]) == permu
+                i += 1
+            if not repeated: 
+                samples.add(tuple(permu))
+
+        return  putils.set2np(samples, size) 
--- permu_utils/__init__.py
+++ permu_utils/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+import itertools as it
 
 # def random_permutation(n):
 #     '''Generate a random permutation of the specified length.
@@ -48,4 +49,78 @@ def random_population(n, size):
 
     # return np.array(list(pset)).reshape((size, n))
     return set2np(pset, n)
+
+def discordancesToPermut(indCode, refer):
+    n = len(indCode)
+    rem = refer[:]  #[i for i in refer]
+    sigma = np.zeros(n,dtype=np.int)
+    for i in range(n):
+        sigma[i] = rem[indCode[i]]
+        rem.pop(indCode[i])
+    return sigma#[i+1 for i in permut];
+
+def kendallTau(A, B=None):
+    if B is None : B = list(range(len(A)))
+    pairs = it.combinations(range(0, len(A)), 2)
+    distance = 0
+    for x, y in pairs:
+        #if not A[x]!=A[x] and not A[y]!=A[y]:#OJO no se check B
+        a = A[x] - A[y]
+    try:
+        b = B[x] - B[y]# if discordant (different signs)
+    except:
+        print("ERROR kendallTau, check b",A, B, x, y)
+    # print(b,a,b,A, B, x, y)
+    if (a * b < 0):
+        distance += 1
+    return distance
+
+def permu2vj(permu): 
+    '''Transform a permutation its Vj representation.
+
+    Args:
+        permu (ndarray): Numpy permutation.
+
+    Returns:
+        ndarray: Vj array. 
+    '''
+    vj = []
+    i = 0
+    while len(permu) > 1:
+        # vj.append((permu > permu[0]).sum())
+        vj.append((permu < permu[0]).sum())
+        permu = np.delete(permu, 0)
+        i += 1
+    return np.array(vj)
+
+def vj2permu(vj):
+    '''Transform a vector from Vj space to permutation space. 
+
+    Args:
+        vj (ndarray): Numpy array, Vj vector.
+
+    Returns:
+        ndarray: Permutation vector.
+    '''
+    # e = list(reversed(range(vj.shape[0]+1)))
+    e = list(range(vj.shape[0]+1))
+    permu = []
+    for elem in vj:
+        permu.append(e[elem])        
+        del e[elem]
+    permu.append(e[0])
+    return np.array(permu)
+
+def transform(pop, func):
+    '''Applies a trasformation function to every individual of the population.
+    
+    Args:
+        pop (ndarray): population matrix.
+        func (function): instance of the transformation function.
+
+    Returns:
+        ndarray: Transformated population matrix.
+    '''
+    return np.array(list(map(func, pop)))
+
 
