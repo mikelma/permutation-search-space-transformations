7bdabc7133a5940c4d6ea18f051dd23ea1ddf879
mikelma <mikelma7@gmail.com>
2019-04-23 16:47:08 +0200

    Updated PFSP's load_instance matehod updated to work with convention instances of PFSP

--- problems/PFSP.py
+++ problems/PFSP.py
@@ -3,21 +3,17 @@ import os
 
 class PFSP():
 
-    def __init__(self, size, 
-                 instances_dir='instances/PFSP'):
-        """ PFSP problem initializer.
+    def __init__(self, instances_dir='instances/PFSP'):
+        """PFSP problem initializer.
 
         Args:
-            size (int): Size of the problem, number of jobs
             instances_dir (str): Default: 'instances/PFSP'. Directory where
                                  the PFSP instaces are located.
         """
-        self.size = size
-
         # Chance current working directory to the file where instances are
         os.chdir(instances_dir)
 
-    def load_instance(self, instance_file):
+    def load_instance(self, instance_name):
         """Loads saved PFSP instance.
         
         Args: 
@@ -29,41 +25,63 @@ class PFSP():
         f = open(instance_name, 'r')
         lines = f.readlines()
         f.close()
-        sizes = lines[0].strip('\n').split(',') 
-        n_machines = int(sizes[0]) 
-        n_jobs = int(sizes[1]) 
+        
+        # Find instance size
+        row = []
+        for item in lines[1].split(' '):
+            try:
+                row.append(int(item))
+            except:
+                pass
+        
+        n_jobs = row[0]
+        n_machines = row[1]
 
-        # Distance matrix 
-        d = lines[1].split(' ')  
-        instance = np.empty((n_machines, n_jobs))
-        a = 0
+        lines = lines[3:]
+        
+        instance = np.empty((n_machines, n_jobs), dtype=np.int)
+
+        # Read instance matrix        
         for i in range(n_machines):
+            row_ = lines[i].strip('\n').split(' ')
+            row = []
+            # Check for valid data in the row string
+            for item in row_:
+                try:
+                    row.append(int(item))
+                except:
+                    pass
+
             for j in range(n_jobs):
-                instances[i][j] = float(d[a])
-                a += 1
+                instance[i][j] = row[j]
+
         return instance
 
-    def generate_instance(self, instance_name,
-                          n_machines, n_jobs, 
-                          min_val, max_time):
-        """Generates a file containing a PFSP instance.
-        The instance matrix is randomly generated with the 
-        specified ranges and size. Matrix: machines x jobs. 
+    # def generate_instance(self, instance_name,
+    #                       n_machines, n_jobs, 
+    #                       min_val, max_time):
+    #     """Generates a file containing a PFSP instance.
+    #     The instance matrix is randomly generated with the 
+    #     specified ranges and size. Matrix: machines x jobs. 
 
-        Args:
-            instance_name (str): The name of the output file.
-            n_machines (int): Number of machines of the instance.
-            n_jobs (int): Number of jobs of the instance.
-            min_val (float): Lower range of the instance matrix.
-            max_val (float): Upper range of the instance matrix.
-        """
-        instance = np.random.uniform(min_val, max_val,
-                                     size=(n_machines, n_jobs))
+    #     Args:
+    #         instance_name (str): The name of the output file.
+    #         n_machines (int): Number of machines of the instance.
+    #         n_jobs (int): Number of jobs of the instance.
+    #         min_val (float): Lower range of the instance matrix.
+    #         max_val (float): Upper range of the instance matrix.
+    #     """
+    #     instance = np.random.uniform(min_val, max_val,
+    #                                  size=(n_machines, n_jobs))
 
-    def evaluate():
-        pass
+    # def evaluate():
+    #     pass
 
 if __name__ == '__main__':
 
-    pfsp = PFSP(5)
+    pfsp = PFSP()
     help(pfsp)
+    
+    # a = pfsp.load_instance('tai20_5_0.fsp')
+    # print(a)
+
