d0511887f50e3b5295c4588b0a5f2bff258b0ec1
mikelma <mikelma7@gmail.com>
2019-05-13 10:34:20 +0200

    Read config and run algorithm stable

--- dbman.py
+++ dbman.py
@@ -1,6 +1,13 @@
 import seaborn as sns
 import glob
 import configparser
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+import problems
+
+from algorithm import Algorithm
 
 class DBMan():
     
@@ -17,20 +24,22 @@ class DBMan():
         Args:
             config_f (str or None) : Path where the config file will be created. Default: None.
         '''
-
         if type(config_f) is not str:
             config_f = self.config_f
 
         self.config['MAIN'] = {
+            'repetitions': '1',
             'search space': 'permutation',
             'population size': '200',
             'survivor rate': '0.5',
             'iterations': '400',
-            'timeout': '3'}
+            'check repeat':'True',
+            'timeout': '3000',
+            'permutation dtype': 'int8'}
         
         self.config.add_section('INSTANCE')
         self.config.set('INSTANCE', 'problem', 'QAP')
-        self.config.set('INSTANCE', 'path', 'instances/QAP/tai20_5.dat')
+        self.config.set('INSTANCE', 'path', 'instances/QAP/tai20b.dat')
         self.config.set('INSTANCE', 'size', '20')
 
         with open(config_f, 'w') as configfile:
@@ -51,32 +60,73 @@ class DBMan():
 
         return self.config
 
-    def run(self):
+    def run_experiment(self):
         
         config = self._read_config()
 
         print('[*] Config file read succsessfully.')
-
+        
+        repetitions = int(config['MAIN']['repetitions'])
         space = config['MAIN']['search space']
         pop_size = int(config['MAIN']['population size'])
         surv_rate = float(config['MAIN']['survivor rate'])
         iterations = int(config['MAIN']['iterations'])
+        check_repeat = bool(config['MAIN']['check repeat'])
         timeout = int(config['MAIN']['timeout'])
+        permu_dtype = config['MAIN']['permutation dtype']
 
-        problem = config['INSTANCE']['problem']
+        problem_name = config['INSTANCE']['problem']
         instance_path = config['INSTANCE']['path']
         size = int(config['INSTANCE']['size'])
 
-        assert space in ['vj', 'permutation'], 'Specified search space in '+self.config_f+' is not valid.'
-        
-        if  problem == 'QAP':
-            pass
+        if permu_dtype == 'int8':
+            permu_dtype = np.int8
 
-        elif problem == 'PFSP':
-            pass
+        # import os
+        # print(os.getcwd())
+        # print(instance_path)
+
+        if  problem_name == 'QAP':
+            problem = problems.QAP() # Init problem
+            dist, flow = problem.load_instance(instance_path) # Read instance
+
+            def evaluator(permu):
+                return problem.evaluate(permu, dist, flow) 
+
+
+        elif problem_name == 'PFSP':
+            problem = problems.PFSP() # Init problem
+            instance = problem.load_instance(instance_path) # Read instance
+
+            def evaluator(permu):
+                # NOTE: Set makespan True to optimize PFSP makespan, else TFT will be evaluated
+                return problem.evaluate(permu, instance, makespan=False) 
+
         else:
             print('Problem ', problem, ' found in ', 
                   self.config_f, ' is not a valid problem name')
+
+        alg = Algorithm(size=size,
+                        pop_size=pop_size,
+                        evaluator=evaluator,
+                        surv_rate=surv_rate,
+                        iters=iterations,
+                        space=space,
+                        timeout=timeout,
+                        check_repeat=check_repeat,
+                        permu_dtype=permu_dtype)
+
+        log = alg.run()
+        
+        iters = range(len(log['min']))
+
+        plt.plot(iters, log['min'], label='min')
+        plt.plot(iters, log['max'], label='max')
+        plt.plot(iters, log['median'], label='median')
+        plt.legend()
+
+        plt.show()
+
 if __name__ == '__main__':
 
     dbman = DBMan()
@@ -84,4 +134,4 @@ if __name__ == '__main__':
     if input('[*] Create config? [y/N] ') == 'y':
         dbman.create_config()
 
-    dbman.run()
+    dbman.run_experiment()
