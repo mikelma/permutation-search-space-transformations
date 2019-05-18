c0ecd433844973294912838513b6cadece24d4d5
mikelma <mikelma7@gmail.com>
2019-05-18 21:37:37 +0200

    Cleanup, deleted old sampling function

--- optimizers/UMDA.py
+++ optimizers/UMDA.py
@@ -35,7 +35,6 @@ class UMDA():
 
         return freq
 
-    # @profile
     def sample_population(self, 
                           p, 
                           samples,
@@ -43,7 +42,6 @@ class UMDA():
                           pop,
                           pop_f,
                           eval_func,
-                          permutation,
                           check_repeat,
                           timeout=None):
         '''Given a probability matrix of size nxm, n_samples number of solutions 
@@ -57,101 +55,11 @@ class UMDA():
                            repeated in the sampled matrix. 
             pop_f (ndarray): Fitness array of the given population (pop).
             eval_func: Instance of the evaluation function.
-            permutation (bool): Set true if the solutions to sample are permutations, else False.
             check_repeat (bool): Check if the sampled solution exists in the population, solutions won't be repeated.. 
             timeout (int or None): Enable timeout, in milliseconds. Default: None.
         
         Returns:
             tuple(ndarray, ndarray) : sampled solutions matrix and the fitness array of the sampled solutions. 
-
-        '''
-        size = min(p.shape) # Size of solutions to sample 
-
-        # if not permutation:
-        #     size -= 1
-
-        print('size: ', size)
-
-        # identity = np.array(range(p.shape[0]))
-        identity = np.array(range(size))
-
-        start = datetime.datetime.now()
-        n_sampled = 0 # Number of permutations sampled and added to the new pop 
-
-        while n_sampled < samples.shape[0]:
-
-            ## Watch for timeouts
-            delta_t = datetime.datetime.now() - start
-            if type(timeout) == int and int(delta_t.total_seconds() * 1000) >= timeout:
-                raise TimeoutError('Error: Timeout passed when sampling new solutions.')
-
-            # Generate permu
-            permu = []
-            for j in range(size): # For each position
-                # Probability for elements in the j's position 
-                if permutation:
-                    p_ = np.delete(p[j], permu, axis=0) 
-                    available = np.delete(identity, permu) 
-                else:
-                    p_ = p[j]
-
-                if sum(p_) == 0: 
-                    rand = 0 
-                else:
-                    rand = np.random.uniform(0, sum(p_))
-
-                i = 0
-                s = 0 # sum
-                while s < rand:
-                    s += p_[i] 
-                    if s < rand:
-                        i += 1
-
-                if permutation:
-                    permu.append(available[i])
-                else:
-                    permu.append(identity[i])
-            
-            # If Vjs, transform vj to permu
-            if not permutation:
-                print(len(permu))
-                permu = putils.vj2permu(np.array(permu))
-
-            # Evaluate the sampled permu
-            f = eval_func(permu)
-            
-            # Add the sampled solution to the population 
-            if f not in pop_f and check_repeat:
-                # Check if the sampled solution exists in the population
-                i = 0
-                repeated = False
-                while not repeated and i < pop.shape[0]:
-                    repeated = np.all(pop[i] == permu)
-                    i += 1
-
-                if not repeated:
-                    samples[n_sampled] = permu
-                    samples_f[n_sampled] = f
-                    n_sampled += 1
-
-            elif not check_repeat:
-                # Do not check if the sampled ppulation already exists in pop
-                samples[n_sampled] = permu
-                samples_f[n_sampled] = f
-                n_sampled += 1
-                 
-        return samples, samples_f 
-
-    def sample_population_fast(self, 
-                               p, 
-                               samples,
-                               samples_f,
-                               pop,
-                               pop_f,
-                               eval_func,
-                               check_repeat,
-                               timeout=None):
-        '''Fast version of the sample_population method.
         '''
         size = min(p.shape) # Size of the permutation to sample 
         permutation = p.shape[0] == p.shape[1] # Define search space
@@ -237,3 +145,18 @@ class UMDA():
                 n_sampled += 1
 
         return samples, samples_f 
+
+    def sample_population_v2(self, 
+                          p, 
+                          samples,
+                          samples_f,
+                          pop,
+                          pop_f,
+                          eval_func,
+                          permutation,
+                          check_repeat,
+                          timeout=None):
+        '''New sampling method.
+        '''
+        pass
+
