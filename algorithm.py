a06cdd4d269d37b0cb6114780d5187a9d82411be
mikelma <mikelma7@gmail.com>
2019-05-22 13:34:27 +0200

    Algorithm adapted to new changes in UMDA sampling and probability distribution functions

--- algorithm.py
+++ algorithm.py
@@ -13,6 +13,7 @@ class Algorithm():
                  surv_rate,
                  iters,
                  space,
+                 sampling_func,
                  timeout,
                  check_repeat,
                  permu_dtype=np.int8):
@@ -34,24 +35,32 @@ class Algorithm():
         Returns:
             Algorithm instance.
         '''
-        assert space in ['permutation', 'vj'], 'Please select a valid search space type.'
-
-        self.space = space
-        self.timeout = timeout
         self.check_repeat = check_repeat
-
+        self.timeout = timeout
         self.size = size
         self.pop_size = pop_size
-
         self.evaluate = evaluator
-        
         self.n_surv = int(pop_size*surv_rate)
-
         self.iters = iters
-
         self.permu_dtype = permu_dtype
 
         self.umda = UMDA()
+
+        self.sampling_func = sampling_func
+
+        # Define search space specific variables
+        if space == 'permutation':
+            self.transform = False
+            self.space2permu = None 
+
+        elif space == 'vj':
+            self.transform= True
+            self.permu2space = putils.permu2vj
+            self.space2permu = putils.vj2permu
+
+        else:                                  
+            print('Please select a valid search space type.')
+            quit()
 
     # @profile
     def run(self, verbose=True):
@@ -64,7 +73,6 @@ class Algorithm():
             log (dict): a dictionary with 'min', 'max', 'mean' and 'median' keys,
                         storing data of each generation.
         '''
-
         # Init loggers
         log = {'min':[],
                'max':[],
@@ -92,10 +100,24 @@ class Algorithm():
         for i in range(self.pop_size):
             pop_f[i] = self.evaluate(pop[i])
 
-
         ### MAIN LOOP ###
 
         for iter_ in range(self.iters):
+
+            ############################################
+            #   NOTE: DEBUG
+            ############################################
+            # if not putils.is_permutation(pop):
+            #     best = pop[np.argsort(pop_f)[0]]
+            #     print('Not permu found in pop!')
+            #     print('Best solution: ', best)
+            #     print('space2permu: ', self.space2permu)
+            #     print('Sampling func: ', self.sampling_func)
+            #     print('transformation: ', self.transform)
+            #     quit()
+            # else:
+            #     print('--> Pop Ok!')
+            ############################################
 
             # Log data
             log['min'].append(np.min(pop_f))
@@ -119,39 +141,26 @@ class Algorithm():
                 surv[i] = pop[indx]
                 surv_f[i] = pop_f[indx]
 
-            if self.space == 'permutation':
-                # Learn distribution
-                p = self.umda.learn_distribution(surv,
-                                            shape=(self.size, 
-                                                   self.size)) 
-            elif self.space == 'vj':
-                # Transform survivor permus to vj
-                surv_vj = putils.transform(surv, putils.permu2vj)
 
-                # Learn distribution
-                p = self.umda.learn_distribution(surv_vj,
-                                            shape=(self.size,
-                                                   self.size-1))
+            if self.transform:
+                # Transform survivors
+                surv_transformed = putils.transform(surv, self.permu2space)
 
-            p = p.T # NOTE: Temporary solution
-            # NOTE: If permutation apply laplace, sum 1 to the probability matrix
-            if self.space == 'permutation':
-                p += 1
+                p = self.umda.learn_distribution(surv_transformed)
+            else:
+                p = self.umda.learn_distribution(surv)
             
             # Sample new solutions
-            # try:
-            samples, samples_f = self.umda.sample_population(p=p, 
+            samples, samples_f = self.umda.sample_population_v2(p=p, 
+                                                             sampling_func=self.sampling_func,
                                                              samples=samples,
                                                              samples_f=samples_f,
                                                              pop=pop,
                                                              pop_f=pop_f,
                                                              eval_func=self.evaluate,
+                                                             transformation=self.space2permu,
                                                              check_repeat=self.check_repeat,
                                                              timeout=self.timeout)
-            # except Exception as e:
-            #     print('[!] Timeout exception occurred. Returning log.')
-            #     print(e)
-            #     return log
 
             # Ranking, the best fitness valued solutions index
             ranking = np.argsort(pop_f)
@@ -180,13 +189,6 @@ class Algorithm():
                     stop = True
 
                 i += 1
-        ############################################
-        # best = pop[np.argsort(pop_f)[0]]
-        # print('Best solution: ', best)
-        # if not putils.is_permutation(pop):
-        #     print('Not permu found in pop!')
-        #     quit()
-        ############################################
 
         return log
                 
@@ -195,30 +197,35 @@ if __name__ == '__main__':
     from problems import QAP
     from problems import PFSP
     import matplotlib.pyplot as plt
+    from optimizers import UMDA
+
+    umda = UMDA()
     
-    INSTANCE_NAME = 'instances/QAP/tai20b.dat'
-    # INSTANCE_NAME = 'instances/PFSP/tai20_5_0.fsp'
     SIZE = 20
     POP_SIZE = 200
     SURV_RATE = .5
-    ITERS = 200
-    # SPACE = 'permutation'
-    SPACE = 'vj'
+    ITERS = 700
     TIMEOUT = 3*1000
     CHECK_REPEAT = True
     DTYPE = np.int8
 
-    problem = QAP()
-    # problem = PFSP()
+    SPACE = 'permutation'
+    SAMPLING = umda.sample_ad_hoc_laplace 
 
-    dist, flow = problem.load_instance(INSTANCE_NAME)
-    # instance = problem.load_instance(INSTANCE_NAME)
+    # SAMPLING = umda.sample_no_restriction
+    # SPACE = 'vj'
 
-    def evaluate(permu):
-        return problem.evaluate(permu, dist, flow)
-
+    # INSTANCE_NAME = 'instances/QAP/tai20b.dat'
+    # problem = QAP()
+    # dist, flow = problem.load_instance(INSTANCE_NAME)
     # def evaluate(permu):
-    #     return problem.evaluate(permu, instance)
+    #     return problem.evaluate(permu, dist, flow)
+    
+    INSTANCE_NAME = 'instances/PFSP/tai20_5_0.fsp'
+    problem = PFSP()
+    instance = problem.load_instance(INSTANCE_NAME)
+    def evaluate(permu):
+        return problem.evaluate(permu, instance)
 
     alg = Algorithm(size=SIZE,
                     pop_size=POP_SIZE,
@@ -226,6 +233,7 @@ if __name__ == '__main__':
                     surv_rate=SURV_RATE,
                     iters=ITERS,
                     space=SPACE,
+                    sampling_func=SAMPLING,
                     timeout=TIMEOUT,
                     check_repeat=CHECK_REPEAT,
                     permu_dtype=DTYPE)
